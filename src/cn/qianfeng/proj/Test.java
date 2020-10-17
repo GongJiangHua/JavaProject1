f90ce5ea914ef5fde31c0bad8f5acb264299dfe8
GongJiangHua <1914805653@qq.com>
2020-10-17 20:41:11 +0800

    1017java代码提交

--- src/cn/qianfeng/proj/Test.java
+++ src/cn/qianfeng/proj/Test.java
@@ -6,15 +6,15 @@ import java.io.ObjectOutputStream;
 
 
 public class Test {
-    public static void main(String[] args) throws IOException {
-        User u = new User();
-        u.setUsername("龚江华");
-        System.out.println(u.getUsername());
-        /*File file1;
-        file1 = new File("C:\\Users\\GJH20000127\\Desktop\\存储文件\\1011存储");
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        //User u = new User();
+        //u.setUsername("龚江华");
+        //System.out.println(u.getUsername());
+        File file1;
+        file1 = new File("C:\\Users\\GJH20000127\\Desktop\\存储文件\\1013存储");
         final boolean flag1 = file1.createNewFile();
         System.out.println("flag1:"+flag1);
-        File file2 = new File("C:\\Users\\GJH20000127\\Desktop\\存储文件\\a");
+        /*File file2 = new File("C:\\Users\\GJH20000127\\Desktop\\存储文件\\a");
         final boolean flag2 = file2.mkdir();
         System.out.println("flag2"+flag2);
         File file3 = new File("C:\\Users\\GJH20000127\\Desktop\\c\\a\\b");
@@ -23,13 +23,25 @@ public class Test {
         //将类型数据存入text文件
         //存入磁盘
         //文件输出流
-        FileOutputStream fis = new FileOutputStream("C:\\Users\\GJH20000127\\Desktop\\存储文件\\1010存储.txt");
-        //对象输入流
+        /*FileOutputStream fis = new FileOutputStream("C:\\Users\\GJH20000127\\Desktop\\存储文件\\1010存储.txt");
+        //对象输出流
         ObjectOutputStream outputStr =new ObjectOutputStream(fis);
         //开始输出
         outputStr.writeObject(u);
         //关闭流
         outputStr.close();
+        fis.close();*/
+        //要读取的文件路径
+        String path = "C:\\Users\\GJH20000127\\Desktop\\存储文件\\1010存储.txt";
+        //文件输入流
+        FileInputStream fis = new FileInputStream(path);
+        //对象输入流
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        //读取数据，数据类型转换
+        User Obj =(User)ois.readObject();
+        System.out.println(Obj);
+        //关闭流
         fis.close();
+        ois.close();
     }
 }
